9200202d4ccf428543f715b759593abb294b2dc4
Werner Holzapfel <Werner@Werners-MacBook-Pro.local>
2016-06-24 09:12:15 +0200

    uri

--- server.js
+++ server.js
@@ -16,13 +16,18 @@ app.use(bodyParser.json());
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
+var uri = process.env.MONGOLAB_GOLD_URI;
+
+console.log("url: " + uri);
 
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect('mongodb://heroku_kxnktwsj:9cbcg3g2r9defoist09f3vduu1@ds023902.mlab.com:23902/heroku_kxnktwsj', function (err, database) {
+mongodb.MongoClient.connect(process.env.MONGOLAB_GOLD_URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
+
+
 
   // Save database object from the callback for reuse.
   db = database;
