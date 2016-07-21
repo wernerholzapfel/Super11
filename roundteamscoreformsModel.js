4f04c4a584e8077eb2e5ecd1e893f4e3da03f795
Werner Holzapfel <Werner@Werners-MacBook-Pro.local>
2016-07-21 11:11:38 +0200

    new file added

---
+++ roundteamscoreformsModel.js
@@ -0,0 +1,32 @@
+console.log("roundteamscoreforms is ingeladen")
+
+var mongoose = require( 'mongoose' );
+var autoIncrement = require('mongoose-auto-increment');
+
+var roundTeamScoreFormsSchema = new mongoose.Schema({
+	RoundId: { type: Number, required: false },
+	Player: [{
+		Id: { type: Number, required: true },
+        Name: { type: String, required: true },
+        Team: { type: String, required: true },
+        TeamId: { type: Number, required: true },
+        Selected: { type: Boolean, required: true },
+        Position: { type: String, required: true },
+		Played: { type: Boolean, required: false },
+		Win: { type: Boolean, required: false },
+		Draw: { type: Boolean, required: false },
+		Goals: { type: Number, required: false },
+		Assists: { type: Number, required: false },
+		Yellow: { type: Number, required: false },
+		Red: { type: Number, required: false },
+		CleanSheet: { type: Boolean, required: false },
+		OwnGoal: { type: Number, required: false }
+	}]
+});
+
+// autoIncrement.initialize(mongoose);
+// mongoose.plugin(autoIncrement.plugin, { model: 'players', field: 'RoundId' });
+
+var roundteamscoreforms = module.exports = mongoose.model('roundteamscoreforms', roundTeamScoreFormsSchema);
+
+
