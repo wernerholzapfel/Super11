e93d88a410492674d16e28ed20db4a4a111c3d18
Werner Holzapfel <Werner@Werners-MacBook-Pro.local>
2016-08-11 22:29:40 +0200

    new webform initieel

--- public/js/services/registrationApi.js
+++ public/js/services/registrationApi.js
@@ -2,9 +2,9 @@
 angular.module('MetronicApp').service('registrationService', function ($http) {
     //Create new record
     this.post = function (registrationForm) {
-        var req = $http.post('http://localhost:8200/api/predictions', registrationForm);
+        var req = $http.post('https://safe-oasis-58234.herokuapp.com/api/predictions', registrationForm);
         return req;
-// https://safe-oasis-58234.herokuapp.com
+// 
     }
 });
 
@@ -13,7 +13,7 @@ angular.module('MetronicApp').factory('getRegistrationForm', function ($http) {
   var myService = {
     async: function () {
       // $http returns a promise, which has a then function, which also returns a promise
-      var promise = $http.get('http://localhost:8200/api/predictionform').then(function (response) {
+      var promise = $http.get('https://safe-oasis-58234.herokuapp.com/api/predictionform').then(function (response) {
         // The then function here is an opportunity to modify the response
         console.log(response);
         // The return value gets picked up by the then in the controller.
