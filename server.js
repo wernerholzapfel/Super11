2341b735835af2b732f4caa56f13241306990cc5
Werner Holzapfel <Werner@Werners-MacBook-Pro.local>
2016-09-13 17:05:13 +0200

    testje

--- server.js
+++ server.js
@@ -711,21 +711,22 @@ apiRoutes.delete("/headlines/:id", passport.authenticate('jwt', { session: false
   var token = getToken(req.headers);
   if (token) {
     var decoded = jwt.decode(token, config.secret);
-    User.findOne({ name: decoded.name }, function (err, user) {
-      if (err) throw err;
-      if (!user) {
-        return res.status(403).send({ success: false, msg: 'Authentication failed. User not found.' });
-      }
-      if (user.name === "werner.holzapfel@gmail.com" || user.name === 'rverberkt') {
-        Headlines.find({ _id: req.params.id }).remove(function (err, item) {
-          if (err) return handleError(res, err.message, "Failed to delete Item");
-          res.status(200).json(item);
-        })
-      }
-      else {
-        return res.status(403).send({ success: false, msg: 'Niet geautoriseerd om wijziging om headline toe te voegen' })
-      }
-    })
+    console.log("delete is aangeroepen door: " decoded.name);
+//    User.findOne({ name: decoded.name }, function (err, user) {
+//      if (err) throw err;
+//      if (!user) {
+//        return res.status(403).send({ success: false, msg: 'Authentication failed. User not found.' });
+//      }
+//      if (user.name === "werner.holzapfel@gmail.com" || user.name === 'rverberkt') {
+//        Headlines.find({ _id: req.params.id }).remove(function (err, item) {
+//          if (err) return handleError(res, err.message, "Failed to delete Item");
+//          res.status(200).json(item);
+//        })
+//      }
+//      else {
+//        return res.status(403).send({ success: false, msg: 'Niet geautoriseerd om wijziging om headline toe te voegen' })
+//      }
+//    })
   }
 });
 
