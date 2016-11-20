4a07750c80a4304c11c22acb94c8b141598a44a8
werner.holzapfel <werner.holzapfel@trivento.nl>
2016-11-20 22:56:20 +0100

    api endpoint aangepast

--- public/js/services/constants.js
+++ public/js/services/constants.js
@@ -1,10 +1,12 @@
 angular.module('MetronicApp')
- 
-.constant('AUTH_EVENTS', {
-  notAuthenticated: 'auth-not-authenticated'
-})
- 
-.constant('API_ENDPOINT', {
-  url: 'https://safe-oasis-58234.herokuapp.com/api'
-//  url: 'http://localhost:8200/api'
-});
+
+  .constant('AUTH_EVENTS', {
+    notAuthenticated: 'auth-not-authenticated'
+  })
+
+  .constant('API_ENDPOINT', {
+    // url: 'https://safe-oasis-58234.herokuapp.com/api'
+    //  url: 'http://localhost:8200/api'
+    url: '/api'
+
+  });
