da8b9cbfbd96d0c485ac10fa4efc907a4dffda4d
werner.holzapfel <werner.holzapfel@trivento.nl>
2017-01-22 11:00:18 +0100

    used new logo

--- server.js
+++ server.js
@@ -20,17 +20,17 @@ var _ = require('lodash');
 var async = require("async");
 
 var allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
 };
 
 dotenv.load();
@@ -39,8 +39,8 @@ app.use(allowCrossDomain);
 app.use(express.static(__dirname + "/public"));
 
 // get our request parameters
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 //log to consolea
 app.use(morgan('dev'));
@@ -50,15 +50,15 @@ app.use(passport.initialize());
 
 // Initialize the app.
 var server = app.listen(process.env.PORT || 8200, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
+    var port = server.address().port;
+    console.log("App now running on port", port);
 });
 
 require('./config/passport')(passport);
 
 var authenticateCall = jwt({
-  secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
-  audience: process.env.AUTH0_CLIENT_ID
+    secret: new Buffer(process.env.AUTH0_CLIENT_SECRET, 'base64'),
+    audience: process.env.AUTH0_CLIENT_ID
 });
 
 var apiRoutes = express.Router();
@@ -76,7 +76,7 @@ var homepagestats = require('./api/homepagestats');
 var scoreforms = require('./api/scoreforms');
 var standen = require('./api/standen');
 var totalscoreperuser = require('./api/totalscoreperuser');
-var headlines =require('./api/headlines');
+var headlines = require('./api/headlines');
 var postheadlines = require('./api/postheadlines');
 var statistieken = require('./api/statistieken');
 var eredivisieplayers = require('./api/eredivisieplayers');
@@ -86,25 +86,25 @@ var rounds = require('./api/rounds');
 var getlatestteam = require('./api/getlatestteam');
 var savetransfers = require('./api/savetransfers');
 
-app.use('/api',homepagestats,comments,standen,headlines,statistieken,eredivisieplayers,predictions,rounds,istransfermarktopen);
-app.use('/api',authenticateCall,totalscoreperuser,scoreforms,postheadlines,postcomments,getlatestteam,savetransfers);
+app.use('/api', homepagestats, comments, standen, headlines, statistieken, eredivisieplayers, predictions, rounds, istransfermarktopen);
+app.use('/api', authenticateCall, totalscoreperuser, scoreforms, postheadlines, postcomments, getlatestteam, savetransfers);
 
 
 getToken = function (headers) {
-  if (headers && headers.authorization) {
-    var parted = headers.authorization.split(' ');
-    if (parted.length === 2) {
-      return parted[1];
+    if (headers && headers.authorization) {
+        var parted = headers.authorization.split(' ');
+        if (parted.length === 2) {
+            return parted[1];
+        } else {
+            return null;
+        }
     } else {
-      return null;
+        return null;
     }
-  } else {
-    return null;
-  }
 };
 
 // Generic error handler used by all endpoints.
-handleError = function(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({ "error": message });
+handleError = function (res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
 };
