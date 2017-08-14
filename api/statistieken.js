eed03975379e94a97234300d66b9f945461e6b7c
werner.holzapfel <werner.holzapfel@trivento.nl>
2017-08-14 18:09:31 +0200

    Statistieken werken nu ook

--- api/statistieken.js
+++ api/statistieken.js
@@ -2,59 +2,105 @@ var express = require("express");
 var apiRoutes = express.Router();
 var mongoose = require('mongoose');
 
-var TeamPredictions = require("../models/teamPredictionsModel");
+var Predictions = require("../models/predictionModel");
 
 apiRoutes.get("/teamstatistieken/", function (req, res, next) {
-  TeamPredictions.aggregate([
-    { $sort: { RoundId: -1 } },
-    {
-      $group: {
-        _id: {
-          email: "$Participant.Email",
-          playerName: "$TeamScores.Name",
+    Predictions.aggregate([
 
-        },
-        RoundId: { $first: '$RoundId' },
-        Participant: { $first: "$Participant" },
-        Formation: { $first: "$Formation" },
-        CaptainId: { $first: "$CaptainId" },
-        Team: { $first: "$Team" },
-      },
-    },
-    { $unwind: "$Team" },
-    {
-      $group: {
-        _id: {
-          playerId: "$Team.PlayerId"
-        },
-        Count: { $sum: 1 },
-        Team: { $first: "$Team.Team" },
-        Position: { $first: "$Team.Position" },
-        PlayerName: { $first: "$Team.PlayerName" },
-        PlayerId: { $first: "$Team.PlayerId" },
-      }
-    }
-    , {
-      $project:
-      {
-        _id: 0,
-        Count: 1,
-        Team: 1,
-        Position: 1,
-        PlayerName: 1,
-        PlayerId: 1
-      }
-    },
-    { $sort: { Count: -1 } }
-  ], function (err, teamstatistieken) {
-    if (err) {
-      handleError(res, err.message, "failed to get teamstatistieke ")
-    }
-    else {
-      res.status(200).json(teamstatistieken);
-    }
-  }
-  )
+            {$unwind: "$Team"},
+            {
+                $group: {
+                    _id: {
+                        playerId: "$Team.PlayerId"
+                    },
+                    Count: {$sum: 1},
+                    Team: {$first: "$Team.Team"},
+                    Position: {$first: "$Team.Position"},
+                    PlayerName: {$first: "$Team.PlayerName"},
+                    PlayerId: {$first: "$Team.PlayerId"},
+                }
+            }
+            , {
+                $project: {
+                    _id: 0,
+                    Count: 1,
+                    Team: 1,
+                    Position: 1,
+                    PlayerName: 1,
+                    PlayerId: 1
+                }
+            },
+            {$sort: {Count: -1}}
+        ], function (err, teamstatistieken) {
+            if (err) {
+                handleError(res, err.message, "failed to get teamstatistieke ")
+            }
+            else {
+                res.status(200).json(teamstatistieken);
+            }
+        }
+    )
 });
 
 module.exports = apiRoutes;
+
+// var express = require("express");
+// var apiRoutes = express.Router();
+// var mongoose = require('mongoose');
+//
+// var TeamPredictions = require("../models/teamPredictionsModel");
+//
+// apiRoutes.get("/teamstatistieken/", function (req, res, next) {
+//   TeamPredictions.aggregate([
+//     { $sort: { RoundId: -1 } },
+//     {
+//       $group: {
+//         _id: {
+//           email: "$Participant.Email",
+//           playerName: "$TeamScores.Name",
+//
+//         },
+//         RoundId: { $first: '$RoundId' },
+//         Participant: { $first: "$Participant" },
+//         Formation: { $first: "$Formation" },
+//         CaptainId: { $first: "$CaptainId" },
+//         Team: { $first: "$Team" },
+//       },
+//     },
+//     { $unwind: "$Team" },
+//     {
+//       $group: {
+//         _id: {
+//           playerId: "$Team.PlayerId"
+//         },
+//         Count: { $sum: 1 },
+//         Team: { $first: "$Team.Team" },
+//         Position: { $first: "$Team.Position" },
+//         PlayerName: { $first: "$Team.PlayerName" },
+//         PlayerId: { $first: "$Team.PlayerId" },
+//       }
+//     }
+//     , {
+//       $project:
+//       {
+//         _id: 0,
+//         Count: 1,
+//         Team: 1,
+//         Position: 1,
+//         PlayerName: 1,
+//         PlayerId: 1
+//       }
+//     },
+//     { $sort: { Count: -1 } }
+//   ], function (err, teamstatistieken) {
+//     if (err) {
+//       handleError(res, err.message, "failed to get teamstatistieke ")
+//     }
+//     else {
+//       res.status(200).json(teamstatistieken);
+//     }
+//   }
+//   )
+// });
+//
+// module.exports = apiRoutes;
